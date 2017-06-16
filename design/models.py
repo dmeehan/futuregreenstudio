4726e57383d84983890e8b786c1f25419b5d270f
Douglas Meehan <dmeehan@gmail.com>
2017-06-16 09:42:45 -0400

    grids for projects and employees

--- design/models.py
+++ design/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django import forms
 from django.db import models
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.tags import ClusterTaggableManager
@@ -23,6 +24,39 @@ class DesignPage(Page):
 
     subpage_types = ['design.ProjectPage']
     parent_page_types = ['home.HomePage']
+
+    def get_context(self, request):
+        pagination_num = 19
+        context = super(DesignPage, self).get_context(request)
+        
+        category = request.GET.get('category')
+
+        if category:
+            context['search_category'] = category
+            all_projects = ProjectPage.objects.live().child_of(self).filter(categories__name=category).order_by('-date')
+        else:
+            all_projects = ProjectPage.objects.live().child_of(self).order_by('-date')
+
+        paginator = Paginator(all_projects, pagination_num) # Show 19 projects per page
+
+        page = request.GET.get('page')
+        try:
+            projects = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            projects = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            projects = paginator.page(paginator.num_pages)
+
+        placeholder_count = range(pagination_num - projects.object_list.count())
+
+        # make the variable 'projects' available on the template
+        context['projects'] = projects
+        context['placeholder_count'] = placeholder_count
+
+        return context
+
 
     @classmethod
     def can_create_at(cls, parent):
