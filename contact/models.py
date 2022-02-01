1a3c5b874e49f91c06837a99454f55283f327f83
Douglas Meehan <dmeehan@gmail.com>
2022-02-01 09:17:07 -0500

    added job posting

--- contact/models.py
+++ contact/models.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from django.db import models
 
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsearch import index
@@ -16,9 +17,11 @@ class ContactPage(Page):
     image = models.ForeignKey(
         'wagtailimages.Image', on_delete=models.PROTECT, related_name='+', help_text="Minimum 1600px wide & 608px tall"
     )
+    job_posting = RichTextField(features=['bold', 'italic', 'link'], blank=True, null=True)
 
     content_panels = Page.content_panels + [
         ImageChooserPanel('image'),
+        FieldPanel('job_posting'),
     ]
 
     subpage_types = []
